fe9e5f78b2f6fde225c4e008cee36ae2f9577334
Andrés Aboytes <aaraboytes@gmail.com>
2021-01-31 09:53:24 -0600

    Bike controlls implementation staterd

---
+++ Assets/ProjectAssets/Scenes/GameplayTesting/BikesTesting/BikeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BikeController : MonoBehaviour
+{
+    [Header("Forward movement")]
+    [SerializeField] float _speed;
+    [SerializeField] float _acceleration;
+    [Header("Rotation")]
+    [SerializeField] float _angleSpeed;
+    [Header("Traceback")]
+    [SerializeField] BikeTraceback _traceback;
+    private Rigidbody body;
+    private float wheelAngle = 0;
+    private float maxWheelAngle = 70f;
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        if(body.velocity.magnitude < _speed)
+            body.AddForce(transform.forward * _acceleration, ForceMode.Acceleration);
+        transform.Rotate(transform.up, horizontalAxis * _angleSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/ProjectAssets/Scenes/GameplayTesting/BikesTesting/BikeTraceback.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BikeTraceback : MonoBehaviour
+{
+    [SerializeField] Transform _tracebackOrigin;
+    [SerializeField] GameObject _tracebackObject;
+    [SerializeField] Vector3 _tracebackDetectorSize;
+    [SerializeField] LayerMask _traceLayer;
+
+    private List<GameObject> tracebackElements = new List<GameObject>();
+
+    private void OnDrawGizmosSelected()
+    {
+        if (_tracebackOrigin)
+            Gizmos.DrawCube(_tracebackOrigin.position, _tracebackDetectorSize);
+    }
+
+    private void Update()
+    {
+        if (Physics.OverlapBox(_tracebackOrigin.position, _tracebackDetectorSize,Quaternion.LookRotation(_tracebackOrigin.forward,Vector3.up),_traceLayer).Length == 0)
+        {
+            GameObject trace = Instantiate(_tracebackObject, _tracebackOrigin.position, Quaternion.LookRotation(_tracebackOrigin.forward, Vector3.up));
+            tracebackElements.Add(trace);
+        }
+    }
+
+    public void Clear()
+    {
+        for (int i = 0; i < tracebackElements.Count; i++)
+        {
+            Destroy(tracebackElements[i]);
+        }
+        tracebackElements.Clear();
+    }
+}
